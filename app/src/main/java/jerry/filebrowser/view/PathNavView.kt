fb4cf698c8c0f3d3230f2396753ca98982c44a3f
Jerry-ojk <jerry.tom.cat@qq.com>
2022-11-11 01:05:29 +0800

    PathNavView转为kotlin

---
+++ app/src/main/java/jerry/filebrowser/view/PathNavView.kt
@@ -0,0 +1,70 @@
+package jerry.filebrowser.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import jerry.filebrowser.R
+import jerry.filebrowser.adapter.PathNavAdapter
+import jerry.filebrowser.adapter.PathNavAdapter.PathNavInterface
+
+class PathNavView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    private val adapter: PathNavAdapter = PathNavAdapter(this)
+    private val runnable = Runnable { scrollToPosition(adapter.itemCount - 1) }
+
+    init {
+        addItemDecoration(ArrowItemDecoration(context))
+        val layoutManager = LinearLayoutManager(context, HORIZONTAL, false)
+        setLayoutManager(layoutManager)
+        setAdapter(adapter)
+    }
+
+    fun setPathNavInterface(pathNavInterface: PathNavInterface?) {
+        adapter.setPathNavInterface(pathNavInterface)
+    }
+
+    fun setLoading(isLoading: Boolean) {
+        adapter.setLoading(isLoading)
+    }
+
+    fun updatePath(absolutePath: String?) {
+        adapter.updatePath(absolutePath)
+        post(runnable)
+    }
+
+    class ArrowItemDecoration(context: Context) : ItemDecoration() {
+        private val arrow: Drawable =
+            ContextCompat.getDrawable(context, R.drawable.ic_action_next)!!
+        private val width: Int = DPUtils.DP(24)
+
+        init {
+            arrow.setTint(context.getColor(R.color.text_subtitle))
+        }
+
+        override fun onDraw(canvas: Canvas, parent: RecyclerView, state: State) {
+            val top = (parent.height - width) / 2
+            val count = parent.childCount - 1
+            for (i in 0 until count) {
+                val left = parent.getChildAt(i + 1).left - width + DPUtils.DP4
+                arrow.setBounds(left, top, left + width, top + width)
+                arrow.draw(canvas)
+            }
+        }
+
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: State) {
+            // int position = parent.getChildAdapterPosition(view);
+            // int count = parent.getAdapter().getItemCount();
+            outRect[0, 0, DPUtils.DP(16)] = 0
+        }
+    }
+}
